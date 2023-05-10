9c2d0bd8455befe41183e4a7d2e70d41a2e6b16e
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-10 11:41:48 -0700

    Created basic gameover screen. Still need to render it properly. Taking a break

---
+++ src/Components/Game/GameOver/index.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { SettingsContext } from '../../../Context/SettingsContext';
+import { Link } from 'react-router-dom';
+
+
+const GameOver = () => {
+    
+const { score, round } = useContext(SettingsContext);
+  
+
+  
+  return (
+        <div>
+            <h1>Game Over</h1>
+            <h2>Score: {score}</h2>
+            <h2>Round: {round}</h2>
+            <Link to="/"><button>Return to Title Screen</button></Link>
+        </div>
+    );
+};
