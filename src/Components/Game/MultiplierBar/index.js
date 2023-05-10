62d6abfbc54d7efa5ddc816acb8700fc511f6d06
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-10 10:02:38 -0700

    Changed the timers to context

--- src/Components/Game/MultiplierBar/index.js
+++ src/Components/Game/MultiplierBar/index.js
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
+import { SettingsContext } from '../../../Context/Settings';
+import './styles.css';
 
 const MultiplierBar = () => {
+
+  
+
   return (
     <>
-      
+    
     </>
   );
-}
+};
 
 export default MultiplierBar;
