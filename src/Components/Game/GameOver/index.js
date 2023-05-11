ffbe560642eaa4d1bfbde6bd7faee7be1595414f
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-10 17:14:53 -0700

    Added basic styling to gameover Screen and added a incorrect letter counter

--- src/Components/Game/GameOver/index.js
+++ src/Components/Game/GameOver/index.js
@@ -1,21 +1,39 @@
 import React, { useContext } from 'react';
 import { SettingsContext } from '../../../Context/Settings';
 import { Link } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 
 const GameOver = () => {
-    
-const { score, round } = useContext(SettingsContext);
-  
 
-  
-  return (
-        <div>
+    const { score, round, incorrectLetters } = useContext(SettingsContext);
+
+
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '400px', // Adjust the height as per your requirement
+                width: '300px', // Adjust the width as per your requirement
+                textAlign: 'center',
+                border: '1px solid black',
+                borderRadius: '10px',
+                margin: '200px auto', // Adjust the margin as per your requirement
+                padding: '20px', // Optional: Add padding to give some space between the content and border
+            }}
+        >
             <h1>Game Over</h1>
             <h2>Score: {score}</h2>
             <h2>Round: {round}</h2>
-            <Link to="/"><button>Return to Title Screen</button></Link>
-        </div>
+            <h2>Incorrect Letters: {incorrectLetters}</h2>
+            <Link to="/">
+                <button>Return to Title Screen</button>
+            </Link>
+        </Box>
     );
 };
 
