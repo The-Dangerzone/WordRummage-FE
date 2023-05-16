0c1f71fc5f9bd5ddea947b6597b494f461ed5c5f
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-16 12:51:32 -0700

    Added Styling to the event log

--- src/Components/Game/EventTracker/index.js
+++ src/Components/Game/EventTracker/index.js
@@ -15,32 +15,30 @@ const EventTracker = () => {
   const lettersMatrix = selectedRoundData ? selectedRoundData[0].letters : [];
 
   return (
-    <Grid container spacing={2}>
-      <Grid item>
-        <Select value={selectedRound}  sx={{width: '130px'}} onChange={handleRoundChange}>
+
+
+    <div className="GO-board-window">
+      <div className='GO-header'>
+        <Select value={selectedRound} sx={{ width: '130px' }} onChange={handleRoundChange}>
           {eventLog.map((roundData, index) => (
             <MenuItem key={index} value={roundData[0].round}>
               Round {roundData[0].round}
             </MenuItem>
           ))}
         </Select>
-      </Grid>
-
-      <Grid item>
-        <div className="GO-board-window">
         <div className="GO-target-word">Target Word: {`${selectedRoundData[0].targetWord}`}</div>
-          <div className="GO-game-container">
-            {lettersMatrix.map((row, i) => (
-              <div key={`row-${i}`} className="GO-letter-row">
-                {row.map((obj, j) => (
-                  <div key={`block-${i}-${j}`} id={`block-${i}-${j}`} className={`GO-${obj.isTarget}`}>{obj.letter}</div>
-                ))}
-              </div>
+      </div>
+      <div className="GO-game-container">
+        {lettersMatrix.map((row, i) => (
+          <div key={`row-${i}`} className="GO-letter-row">
+            {row.map((obj, j) => (
+              <div key={`block-${i}-${j}`} id={`block-${i}-${j}`} className={`GO-${obj.isTarget}`}>{obj.letter}</div>
             ))}
           </div>
-        </div>
-      </Grid>
-    </Grid>
+        ))}
+      </div>
+    </div>
+
   );
 }
 export default EventTracker;
