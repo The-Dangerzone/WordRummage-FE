658640f555883afdcbb61435a845e827469c8c9a
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-16 10:54:35 -0700

    Added Event logs

--- src/Components/Game/GameOver/index.js
+++ src/Components/Game/GameOver/index.js
@@ -1,41 +1,47 @@
 import React, { useContext } from 'react';
 import { SettingsContext } from '../../../Context/Settings';
+import EventTracker from '../EventTracker';
 import { Link } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-
 const GameOver = () => {
+  const { score, round, incorrectLetters, streak, maxStreak } = useContext(SettingsContext);
 
-    const { score, round, incorrectLetters, streak, maxStreak } = useContext(SettingsContext);
-
-
-
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '400px', 
-                width: '300px', 
-                textAlign: 'center',
-                border: '1px solid black',
-                borderRadius: '10px',
-                margin: '200px auto', 
-                padding: '20px', 
-            }}
-        >
-            <h1>Game Over</h1>
-            <h2>Score: {score}</h2>
-            <h2>Round: {round}</h2>
-            <h2>Incorrect Letters: {incorrectLetters}</h2>
-            <h2>Biggest Streak: {maxStreak}</h2>
-            <Link to="/">
-                <button>Return to Title Screen</button>
-            </Link>
-        </Box>
-    );
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          border: '1px solid black',
+          borderRadius: '10px',
+          padding: '20px',
+        }}
+      >
+        <h1>Game Over</h1>
+        <h2>Score: {score}</h2>
+        <h2>Round: {round}</h2>
+        <h2>Incorrect Letters: {incorrectLetters}</h2>
+        <h2>Biggest Streak: {maxStreak}</h2>
+        <Link to="/">
+          <button>Return to Title Screen</button>
+        </Link>
+      </div>
+      <div style={{ marginLeft: '20px' }}>
+        <EventTracker />
+      </div>
+    </div>
+  );
 };
 
 export default GameOver;
