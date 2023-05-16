bd82b071f6f9907e79756e80a895843570d898db
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-16 15:21:19 -0700

    Styled the event log more and added a button

--- src/Components/Game/EventTracker/index.js
+++ src/Components/Game/EventTracker/index.js
@@ -1,6 +1,6 @@
 import { useContext, useState } from 'react';
 import { SettingsContext } from '../../../Context/Settings';
-import { Select, MenuItem, Grid } from '@mui/material';
+import { Select, MenuItem } from '@mui/material';
 import './styles.css';
 
 const EventTracker = () => {
@@ -19,7 +19,7 @@ const EventTracker = () => {
 
     <div className="GO-board-window">
       <div className='GO-header'>
-        <Select value={selectedRound} sx={{ width: '130px' }} onChange={handleRoundChange}>
+        <Select value={selectedRound} sx={{ width: '130px' }} onChange={handleRoundChange} MenuProps={{PaperProps: { style: { maxHeight: 200 }}}}>
           {eventLog.map((roundData, index) => (
             <MenuItem key={index} value={roundData[0].round}>
               Round {roundData[0].round}
