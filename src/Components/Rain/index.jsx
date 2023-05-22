91832827e288aae059f11be5204980b576a56d10
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-22 10:27:54 -0700

    Refactored Rain into a component and updated Title Screen

---
+++ src/Components/Rain/index.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+
+
+function Rain () {
+  const [fallingLetters, setFallingLetters] = useState([]);
+
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  useEffect(() => {
+    const generateFallingLetters = () => {
+      const letters = [];
+      for (let i = 0; i < 100; i++) {
+        const letter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        const delay = Math.random() * 5;
+        const position = Math.random() * 100;
+        const speed = Math.random() * 10 + 1;
+        const fallingLetter = { letter, delay, position, speed };
+        if (!checkCollision(letters, fallingLetter)) {
+          letters.push(fallingLetter);
+        }
+      }
+      setFallingLetters(letters);
+    };
+
+    const checkCollision = (existingLetters, newLetter) => {
+      const collisionThreshold = 1.5; 
+
+      for (const letter of existingLetters) {
+        const distance = Math.abs(letter.position - newLetter.position);
+        if (distance < collisionThreshold) {
+          return true;
+        }
+      }
+
+      return false;
+    };
+
+    generateFallingLetters();
+  }, []);
+
+  return(
+    <div className="falling-letters-container">
+        {fallingLetters.map((fallingLetter, index) => (
+          <span
+            key={index}
+            className="falling-letter"
+            style={{
+              animationDelay: `${fallingLetter.delay}s`,
+              left: `${fallingLetter.position}%`,
+              animationDuration: `${10 / fallingLetter.speed}s`,
+            }}
+          >
+            {fallingLetter.letter}
+          </span>
+        ))}
+      </div>
+  )
+}
+
+export default Rain;
--- src/Components/TitleScreen/index.jsx
+++ src/Components/TitleScreen/index.jsx
@@ -1,45 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { SettingsContext } from "../../Context/Settings";
 import { Link } from "react-router-dom";
+import Rain from "../Rain";
 import "./styles.css";
 
 const TitleScreen = (props) => {
   const { isLoggedIn, setIsLoggedIn } = useContext(SettingsContext);
-  const [fallingLetters, setFallingLetters] = useState([]);
-
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  useEffect(() => {
-    const generateFallingLetters = () => {
-      const letters = [];
-      for (let i = 0; i < 100; i++) {
-        const letter = alphabet[Math.floor(Math.random() * alphabet.length)];
-        const delay = Math.random() * 5;
-        const position = Math.random() * 100;
-        const speed = Math.random() * 10 + 1;
-        const fallingLetter = { letter, delay, position, speed };
-        if (!checkCollision(letters, fallingLetter)) {
-          letters.push(fallingLetter);
-        }
-      }
-      setFallingLetters(letters);
-    };
-
-    const checkCollision = (existingLetters, newLetter) => {
-      const collisionThreshold = 1.5; // Adjust this value to define the minimum distance between letters
-
-      for (const letter of existingLetters) {
-        const distance = Math.abs(letter.position - newLetter.position);
-        if (distance < collisionThreshold) {
-          return true;
-        }
-      }
-
-      return false;
-    };
-
-    generateFallingLetters();
-  }, []);
 
   const handleSinglePlayer = () => {
     // Placeholder in case we need it
@@ -63,21 +29,7 @@ const TitleScreen = (props) => {
 
   return (
     <div className="title-screen-container">
-      <div className="falling-letters-container">
-        {fallingLetters.map((fallingLetter, index) => (
-          <span
-            key={index}
-            className="falling-letter"
-            style={{
-              animationDelay: `${fallingLetter.delay}s`,
-              left: `${fallingLetter.position}%`,
-              animationDuration: `${10 / fallingLetter.speed}s`,
-            }}
-          >
-            {fallingLetter.letter}
-          </span>
-        ))}
-      </div>
+      <Rain />
       <div className="title-screen-content">
         <h1>Word Rummage</h1>
         <div className="button-group">
