c10b05ac48787d347562459fa5ad23c1da4f8723
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-23 10:38:58 -0700

    Changed Slider to it's own component and now have each mode changing state

--- src/Components/Settings/Game/index.jsx
+++ src/Components/Settings/Game/index.jsx
@@ -5,6 +5,7 @@ import { SettingsContext } from '../../../Context/Settings';
 import './styles.css'
 import Rain from '../../Rain';
 import InfoOutlined from '@mui/icons-material/InfoOutlined';
+import BoardSizeSlider from './slider';
 
 
 
@@ -13,21 +14,36 @@ const GameSettings = () => {
   const { setBoardSize, displayTimer, displayRoundTimer, setDisplayTimer, setDisplayRoundTimer, setDisplayScore, displayScore, allowBoardGrowth, setAllowBoardGrowth, resetGame } = useContext(SettingsContext);
 
   const [selectedContainer, setSelectedContainer] = useState(null);
-  const [selectedContainerIndex, setSelectedContainerIndex] = useState(null);
-  
+  const [selectedMode, setSelectedMode] = useState(0);
+
   useEffect(() => {
     resetGame();
   }, []);
 
+
   const handleContainerClick = (index) => {
-    if (selectedContainerIndex === index) {
-      // Clicked on the already selected container, deselect it
-      setSelectedContainer(null);
-      setSelectedContainerIndex(null);
-    } else {
-      // Clicked on a different container, update the selection
-      setSelectedContainer(index);
-      setSelectedContainerIndex(index);
+    if (index === 0) {
+      setSelectedContainer(0);
+      setSelectedMode(0);
+
+    }
+    if (index === 1) {
+      setSelectedContainer(1);
+      setSelectedMode(1);
+      setDisplayTimer(true);
+      setDisplayRoundTimer(true);
+      setDisplayScore(true);
+      setAllowBoardGrowth(true);
+      setBoardSize(6);
+    }
+    if (index === 2) {
+      setSelectedContainer(2);
+      setSelectedMode(2);
+      setDisplayTimer(true);
+      setDisplayRoundTimer(false);
+      setDisplayScore(true);
+      setAllowBoardGrowth(true);
+      setBoardSize(8);
     }
   };
 
@@ -51,6 +67,7 @@ const GameSettings = () => {
                     checked={displayRoundTimer}
                     onChange={(e) => setDisplayRoundTimer(e.target.checked)}
                   />}
+                disabled={selectedMode !== 0}
                 label="Round Timer"
               />
               <Tooltip title="If enabled, the circular progress will be present. If the circle completes the round is over.">
@@ -64,6 +81,8 @@ const GameSettings = () => {
                     checked={displayTimer}
                     onChange={(e) => setDisplayTimer(e.target.checked)}
                   />}
+                disabled={selectedMode !== 0}
+
                 label="Game Over Timer"
               />
               <Tooltip title="If enabled, a timer will be present and the game will end when the timer reaches 0.">
@@ -77,6 +96,8 @@ const GameSettings = () => {
                     checked={displayScore}
                     onChange={(e) => setDisplayScore(e.target.checked)}
                   />}
+                disabled={selectedMode !== 0}
+
                 label="Score"
               />
               <Tooltip title="If enabled, the game will keep a score and will have a score multiplier">
@@ -90,6 +111,8 @@ const GameSettings = () => {
                     checked={allowBoardGrowth}
                     onChange={(e) => setAllowBoardGrowth(e.target.checked)}
                   />}
+                disabled={selectedMode !== 0}
+
                 label="Growing Board" />
               <Tooltip title="If enabled, the board will grow by 1 row and 1 column every 5 rounds.">
                 <InfoOutlined fontSize='small' />
@@ -98,22 +121,15 @@ const GameSettings = () => {
 
             <Box sx={{ width: 300, margin: 1 }}>
               <div style={{ display: 'flex', width: '225px', justifyContent: 'space-between', }}>
-                <Typography id="discrete-slider" gutterBottom>
+                <Typography id="discrete-slider" gutterBottom className={selectedMode !== 0 ? "disabled-typography" : ""}
+                >
                   Starting Board Size (6-20)
                 </Typography>
                 <Tooltip title="The starting size of the board. This number will represent the number of letters per column and row" sx={{ marginTop: '-3px', marginLeft: '-5px' }}>
                   <InfoOutlined fontSize='small' />
                 </Tooltip>
               </div>
-              <Slider
-                defaultValue={6}
-                valueLabelDisplay="auto"
-                step={1}
-                marks
-                min={6}
-                max={20}
-                onChange={(e, value) => setBoardSize(value)}
-              />
+              <BoardSizeSlider/>
             </Box>
           </FormGroup>
 
