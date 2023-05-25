c526f40bbdf2a7af190435a2c26dd2e2a3f2bb71
Jordan Covington <Atlantafan1087@gmail.com>
2023-05-25 11:43:43 -0700

    Added Fading Effect

--- src/Components/Game/CountDown/index.jsx
+++ src/Components/Game/CountDown/index.jsx
@@ -3,29 +3,33 @@ import { useContext, useEffect, useState } from 'react';
 
 import { SettingsContext } from '../../../Context/Settings';
 
-
-
 const CountDown = () => {
-  
-    const { setCountDownFlag } = useContext(SettingsContext);
-  
-    const [countDownNumber, setCountDownNumber] = useState(3);
-  
-    useEffect(() => {
-      if (countDownNumber > 0) {
+  const { setCountDownFlag } = useContext(SettingsContext);
+  const [countDownNumber, setCountDownNumber] = useState(3);
+  const [showNumber, setShowNumber] = useState(true);
+
+  useEffect(() => {
+    if (countDownNumber > 0) {
+      const timeout = setTimeout(() => {
+        setShowNumber(false);
         setTimeout(() => {
           setCountDownNumber(countDownNumber - 1);
-        }, 1500);
-      } else {
-        setCountDownFlag(false);
-      }
-    }, [countDownNumber]);
-  
-    return (
-      <div className='count-down-container'>
-        <h1 className='count-down-number'>{countDownNumber}</h1>
+          setShowNumber(true);
+        }, 500);
+      }, 1500);
+      return () => clearTimeout(timeout);
+    } else {
+      setCountDownFlag(false);
+    }
+  }, [countDownNumber, setCountDownFlag]);
+
+  return (
+    <div className='count-down-container'>
+      <div className={`count-down-number ${showNumber ? 'show' : 'hide'}`}>
+        {countDownNumber}
       </div>
-    );
+    </div>
+  );
 };
 
 export default CountDown;
