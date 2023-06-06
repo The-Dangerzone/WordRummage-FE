c1af4aa89822a29f6c2fa3446a6dadba7764afa7
Jordan Covington <Atlantafan1087@gmail.com>
2023-06-06 15:37:41 -0700

    Added under development pages to leaderboard and instruction pages

--- src/Components/Leaderboard/index.js
+++ src/Components/Leaderboard/index.js
@@ -1,12 +1,36 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import click from "../../assets/audio/button_click.mp3";
+import Rain from '../Rain';
+import { SettingsContext } from '../../Context/Settings';
+import './styles.css'
+
+const clickAudio = new Audio(click);
 
 const Leaderboard = () => {
-  return (
-    <>
-      <p>Leaderboard Page</p>
-      <Link to="/"><button>Back</button></Link>
 
-    </>
+  const { effectVolume } = useContext(SettingsContext);
+
+  const handleClick = () => {
+    clickAudio.currentTime = 0;
+    clickAudio.volume = effectVolume / 100;
+    clickAudio.play();
+  };
+
+  return (
+    <div className='leaderboard-container'>
+      <div className='leaderboard-rain-container'>
+        <Rain />
+      </div>
+      <div className='leaderboard-title'>
+        <h1>Leaderboard</h1>
+      </div>
+      <div className='under-development'>
+        <h1>Currently Under Development</h1>
+      </div>
+
+      <Link to="/"><button onClick={handleClick}>Back</button></Link>
+    </div>
   );
 }
 
