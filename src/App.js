dd9cc7cc17b7f27fca5c7296e0ab4ad980c96451
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-06-14 13:14:36 -0700

    post user

--- src/App.js
+++ src/App.js
@@ -7,8 +7,48 @@ import Instructions from './Components/Instructions';
 import GameScreen from './Components/Game/GameScreen';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  const { user, isAuthenticated, getIdTokenClaims } = useAuth0();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+          console.log(user);
+          postUser();
+          
+      }
+  }, [isAuthenticated])
+  
+
+  const postUser = async () => {
+    if (isAuthenticated) {
+      const res = await getIdTokenClaims();
+      const jwt = res.__raw;
+      
+      const config = {
+        headers: { "Authorization": `Bearer ${jwt}` },
+        method: "post",
+        baseURL: process.env.REACT_APP_SERVER,
+        url: '/user',
+        
+      }
+      try {
+
+        let userFromDB = await axios(config);
+        this.setState({
+          user: userFromDB.data
+        })
+
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+
 
   return (
     <div>
