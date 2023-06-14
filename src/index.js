916ddbff9a65d51ead908328f1a75a8043d588a9
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-06-14 13:34:28 -0700

    add user context

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import SettingsProvider from './Context/Settings';
 import { Auth0Provider } from '@auth0/auth0-react';
+import UserProvider from './Context/User';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,9 +15,11 @@ root.render(
       redirect_uri: window.location.origin
     }}
   >
-    <SettingsProvider>
-      <App />
-    </SettingsProvider>
+    <UserProvider>
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
+    </UserProvider>
 
   </Auth0Provider>
 );
