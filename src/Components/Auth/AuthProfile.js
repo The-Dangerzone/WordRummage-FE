bd84f48d8f185c7d6ca5f1ab8a8ecb82010b0d4f
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-10-30 16:38:29 -0700

    changed profile to displayName

--- src/Components/Auth/AuthProfile.js
+++ src/Components/Auth/AuthProfile.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useContext } from "react";
+import { UserContext } from '../../Context/User';
+
 
 const Profile = () => {
     const { user, isAuthenticated, isLoading } = useAuth0();
+    const { validUser } = useContext(UserContext);
     // if (isAuthenticated) {
     //     console.log(user);
     // }
@@ -15,7 +19,8 @@ const Profile = () => {
         isAuthenticated && (
             <div>
                 <img src={user.picture} alt={user.name} />
-                <h2>{user.name}</h2>
+                {/* <h2>{user.name}</h2> */}
+                <h2>{validUser.displayName}</h2>
                 {/* <p>{user.email}</p> */}
             </div>
         )
