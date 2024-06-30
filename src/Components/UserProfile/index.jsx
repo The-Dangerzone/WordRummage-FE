392879b56ccb2ae335fed58e20dbe511aa6c286f
Jordan Covington <Atlantafan1087@gmail.com>
2024-06-29 22:21:29 -0700

    Created Divs for each mode and styled

--- src/Components/UserProfile/index.jsx
+++ src/Components/UserProfile/index.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { UserContext } from '../../Context/User';
 import { SettingsContext } from "../../Context/Settings";
 import click from "../../assets/audio/button_click.mp3";
-import "./styles.css"
+import "./styles.css";
 import DisplayName from "../Auth/DisplayName";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -12,13 +12,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 const clickAudio = new Audio(click);
 
 const UserProfile = () => {
-
-  // const { validUser } = useContext(UserContext);
   const location = useLocation();
   const { user } = location.state;
   const { effectVolume } = useContext(SettingsContext);
   const { validUser, setValidUser, displayNamePopup, setDisplayNamePopup } = useContext(UserContext);
   const { logout } = useAuth0();
+  const navigate = useNavigate();
 
   const handleClick = () => {
     clickAudio.currentTime = 0;
@@ -29,9 +28,6 @@ const UserProfile = () => {
   const handleChangeName = () => {
     setDisplayNamePopup(true);
   }
-
-
-  const navigate = useNavigate();
 
   const handleDeleteUser = async () => {
     try {
@@ -45,47 +41,55 @@ const UserProfile = () => {
   }
 
   return (
-
     <div className="user-profile">
       <h1>{user.displayName}</h1>
-      {
-        validUser.displayName === user.displayName &&
+      {validUser.displayName === user.displayName && (
         <div>
           <button className='pulse-button' onClick={handleChangeName}>Change Name</button>
           <button className='pulse-button' onClick={handleDeleteUser}>Delete User</button>
         </div>
-      }
+      )}
 
       {displayNamePopup && (
         <DisplayName />
       )}
-      <div className="user-stats">
-        <h2>USER STATS: NORMAL MODE</h2>
-        <div className="user-accuracy">
-          <h3>ACCURACY</h3>
-          <p>CORRECT: {user.normalMode.accuracy.correct}</p>
-          <p>INCORRECT: {user.normalMode.accuracy.incorrect}</p>
-          <p>{user.normalMode.accuracy.percentage}%</p>
-          <h3>OVERALL HIGHSCORE: {user.normalMode.highScore}</h3>
-          <h3>STREAK: {user.normalMode.maxStreak}</h3>
-          <h3>ROUND: {user.normalMode.highestRound}</h3>
+      <div className="user-stats-container">
+        <div className="user-stats">
+          <h2>Normal Mode Stats</h2>
+          <div className="user-accuracy">
+            <p>Correct Letters Clicked: {user.normalMode.accuracy.correct}</p>
+            <p>Incorrect Letters Clicked: {user.normalMode.accuracy.incorrect}</p>
+            <p>Correct Accuracy Percentage: {user.normalMode.accuracy.percentage}%</p>
+            <p>Highest Score: {user.normalMode.highScore}</p>
+            <p>Best Streak: {user.normalMode.maxStreak}</p>
+            <p>Highest Round Reached: {user.normalMode.highestRound}</p>
+          </div>
         </div>
-      </div>
-      <div className="user-stats">
-        <h2>USER STATS: INSANE MODE</h2>
-        <div className="user-accuracy">
-          <h3>ACCURACY</h3>
-          <p>CORRECT: {user.insaneMode.accuracy.correct}</p>
-          <p>INCORRECT: {user.insaneMode.accuracy.incorrect}</p>
-          <p>{user.insaneMode.accuracy.percentage}%</p>
-          <h3>OVERALL HIGHSCORE: {user.insaneMode.highScore}</h3>
-          <h3>STREAK: {user.insaneMode.maxStreak}</h3>
-          <h3>ROUND: {user.insaneMode.highestRound}</h3>
+        <div className="user-stats">
+          <h2>Insane Mode Stats</h2>
+          <div className="user-accuracy">
+          <p>Correct Letters Clicked: {user.insaneMode.accuracy.correct}</p>
+            <p>Incorrect Letters Clicked: {user.insaneMode.accuracy.incorrect}</p>
+            <p>Correct Accuracy Percentage: {user.insaneMode.accuracy.percentage}%</p>
+            <p>Highest Score: {user.insaneMode.highScore}</p>
+            <p>Best Streak: {user.insaneMode.maxStreak}</p>
+            <p>Highest Round Reached: {user.insaneMode.highestRound}</p>
+          </div>
+        </div>
+        <div className="user-stats">
+          <h2>Custom Mode Stats</h2>
+          <div className="user-accuracy">
+          <p>Correct Letters Clicked: {user.customMode.accuracy.correct}</p>
+            <p>Incorrect Letters Clicked: {user.customMode.accuracy.incorrect}</p>
+            <p>Correct Accuracy Percentage: {user.customMode.accuracy.percentage}%</p>
+            <p>Highest Score: {user.customMode.highScore}</p>
+            <p>Best Streak: {user.customMode.maxStreak}</p>
+            <p>Highest Round Reached: {user.customMode.highestRound}</p>
+          </div>
         </div>
       </div>
       <Link to="/"><button className="pulse-button" onClick={handleClick}>Back</button></Link>
     </div>
-
   );
 };
 
